d3986fcd6d18290bcafeedb78e19a94050547243
정핸모 <gusah009@naver.com>
2023-01-04 08:44:37 +0900

    feat: Thumbnail Entity & Repository 추가

---
+++ src/main/java/com/dinosaur/foodbowl/domain/thumbnail/dao/ThumbnailRepository.java
@@ -0,0 +1,8 @@
+package com.dinosaur.foodbowl.domain.thumbnail.dao;
+
+import com.dinosaur.foodbowl.domain.thumbnail.entity.Thumbnail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThumbnailRepository extends JpaRepository<Thumbnail, Long> {
+
+}
--- src/main/java/com/dinosaur/foodbowl/domain/thumbnail/entity/Thumbnail.java
+++ src/main/java/com/dinosaur/foodbowl/domain/thumbnail/entity/Thumbnail.java
@@ -2,23 +2,37 @@ package com.dinosaur.foodbowl.domain.thumbnail.entity;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
+import com.dinosaur.foodbowl.global.entity.BaseEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "thumbnail")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EqualsAndHashCode(of = {"id"})
-public class Thumbnail {
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@ToString(of = {"path"})
+public class Thumbnail extends BaseEntity {
+
+  private static final int MAX_PATH_LENGTH = 512;
 
   @Id
   @GeneratedValue(strategy = IDENTITY)
   @Column(name = "id", nullable = false, updatable = false)
   private Long id;
+
+  @Column(name = "path", nullable = false, length = MAX_PATH_LENGTH)
+  private String path;
+
+  @Builder
+  private Thumbnail(String path) {
+    this.path = path;
+  }
 }
