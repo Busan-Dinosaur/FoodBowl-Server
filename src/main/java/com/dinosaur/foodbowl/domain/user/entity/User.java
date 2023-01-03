7d8d33e052686ffdbe791cf2f4a339a1206faa6c
정핸모 <gusah009@naver.com>
2023-01-03 22:49:17 +0900

    feat: User Entity 생성
    issue: #5

---
+++ src/main/java/com/dinosaur/foodbowl/domain/user/entity/User.java
@@ -0,0 +1,64 @@
+package com.dinosaur.foodbowl.domain.user.entity;
+
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import com.dinosaur.foodbowl.domain.thumbnail.entity.Thumbnail;
+import com.dinosaur.foodbowl.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@ToString(of = {"loginId", "nickname", "introduce"})
+public class User extends BaseEntity {
+
+  public static final int MAX_LOGIN_ID_LENGTH = 40;
+  public static final int MAX_PASSWORD_LENGTH = 512;
+  public static final int MAX_NICKNAME_LENGTH = 40;
+  public static final int MAX_INTRODUCE_LENGTH = 255;
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false)
+  private Long id;
+
+  @JoinColumn
+  @ManyToOne(fetch = LAZY, cascade = ALL)
+  private Thumbnail thumbnail;
+
+  @Column(name = "login_id", nullable = false, unique = true, length = 40)
+  private String loginId;
+
+  @Column(name = "password", nullable = false, length = 512)
+  private String password;
+
+  @Column(name = "nickname", nullable = false, unique = true, length = 40)
+  private String nickname;
+
+  @Column(name = "introduce", unique = true, length = 255)
+  private String introduce;
+
+  @Builder
+  private User(Thumbnail thumbnail, String loginId, String password, String nickname,
+      String introduce) {
+    this.thumbnail = thumbnail;
+    this.loginId = loginId;
+    this.password = password;
+    this.nickname = nickname;
+    this.introduce = introduce;
+  }
+}
---
+++ src/test/java/com/dinosaur/foodbowl/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,103 @@
+package com.dinosaur.foodbowl.domain.user.repository;
+
+import static com.dinosaur.foodbowl.domain.user.entity.User.MAX_INTRODUCE_LENGTH;
+import static com.dinosaur.foodbowl.domain.user.entity.User.MAX_LOGIN_ID_LENGTH;
+import static com.dinosaur.foodbowl.domain.user.entity.User.MAX_NICKNAME_LENGTH;
+import static com.dinosaur.foodbowl.domain.user.entity.User.MAX_PASSWORD_LENGTH;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import com.dinosaur.foodbowl.domain.user.dao.UserRepository;
+import com.dinosaur.foodbowl.domain.user.entity.User;
+import com.dinosaur.foodbowl.global.dao.RepositoryTest;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class UserRepositoryTest extends RepositoryTest {
+
+  @Autowired
+  UserRepository userRepository;
+
+  private User user;
+  private final String loginId = getRandomUUIDLengthWith(MAX_LOGIN_ID_LENGTH);
+  private final String nickname = getRandomUUIDLengthWith(MAX_NICKNAME_LENGTH);
+  private final String password = getRandomUUIDLengthWith(MAX_PASSWORD_LENGTH);
+  private final String introduce = getRandomUUIDLengthWith(MAX_INTRODUCE_LENGTH);
+
+  @BeforeEach
+  void setUpUser() {
+    user = User.builder()
+        .loginId(loginId)
+        .nickname(nickname)
+        .password(password)
+        .introduce(introduce)
+        .build();
+    userRepository.save(user);
+  }
+
+  private static String getRandomUUIDLengthWith(int length) {
+    String randomString = UUID.randomUUID()
+        .toString();
+    length = Math.min(length, randomString.length());
+    return randomString.substring(0, length);
+  }
+
+  @Nested
+  class UniqueColumnTest {
+
+    @Test
+    void should_throwException_when_uniqueColumnIsDuplicate() {
+      assertThatThrownBy(this::generateLoginIdDuplicateUser);
+      assertThatThrownBy(this::generateNicknameDuplicateUser);
+    }
+
+    private void generateLoginIdDuplicateUser() {
+      User duplicateUser = User.builder()
+          .loginId(loginId)
+          .nickname(getRandomUUIDLengthWith(MAX_NICKNAME_LENGTH))
+          .password(getRandomUUIDLengthWith(MAX_PASSWORD_LENGTH))
+          .introduce(getRandomUUIDLengthWith(MAX_INTRODUCE_LENGTH))
+          .build();
+      userRepository.save(duplicateUser);
+    }
+
+    private void generateNicknameDuplicateUser() {
+      User duplicateUser = User.builder()
+          .loginId(getRandomUUIDLengthWith(MAX_LOGIN_ID_LENGTH))
+          .nickname(nickname)
+          .password(getRandomUUIDLengthWith(MAX_PASSWORD_LENGTH))
+          .introduce(getRandomUUIDLengthWith(MAX_INTRODUCE_LENGTH))
+          .build();
+      userRepository.save(duplicateUser);
+    }
+
+    @Test
+    void should_createSuccessfully_when_nonUniqueColumnIsDuplicate() {
+      assertThatNoException().isThrownBy(this::generatePasswordDuplicateUser);
+      assertThatNoException().isThrownBy(this::generateIntroduceDuplicateUser);
+    }
+
+    private void generatePasswordDuplicateUser() {
+      User duplicateUser = User.builder()
+          .loginId(getRandomUUIDLengthWith(MAX_LOGIN_ID_LENGTH))
+          .nickname(getRandomUUIDLengthWith(MAX_NICKNAME_LENGTH))
+          .password(password)
+          .introduce(getRandomUUIDLengthWith(MAX_INTRODUCE_LENGTH))
+          .build();
+      userRepository.save(duplicateUser);
+    }
+
+    private void generateIntroduceDuplicateUser() {
+      User duplicateUser = User.builder()
+          .loginId(getRandomUUIDLengthWith(MAX_LOGIN_ID_LENGTH))
+          .nickname(getRandomUUIDLengthWith(MAX_NICKNAME_LENGTH))
+          .password(getRandomUUIDLengthWith(MAX_PASSWORD_LENGTH))
+          .introduce(introduce)
+          .build();
+      userRepository.save(duplicateUser);
+    }
+  }
+}
