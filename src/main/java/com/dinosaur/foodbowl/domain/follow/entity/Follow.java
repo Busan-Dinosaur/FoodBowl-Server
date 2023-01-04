65338804e9c47a17d7388065975f25e1c3c895bc
woo-chang <clllickme@naver.com>
2023-01-04 20:37:53 +0900

    feat: Follow Entity 구현

---
+++ src/main/java/com/dinosaur/foodbowl/domain/follow/entity/Follow.java
@@ -0,0 +1,43 @@
+package com.dinosaur.foodbowl.domain.follow.entity;
+
+import com.dinosaur.foodbowl.domain.user.entity.User;
+import com.dinosaur.foodbowl.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "follow")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"})
+public class Follow extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "following_id", nullable = false)
+  private User following;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "follower_id", nullable = false)
+  private User follower;
+
+  @Builder
+  public Follow(User following, User follower) {
+    this.following = following;
+    this.follower = follower;
+  }
+}
