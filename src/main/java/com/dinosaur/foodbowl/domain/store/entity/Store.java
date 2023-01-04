25e8bf95fa0cbeb0eca3ba46f85454ef999f02d8
JeonghakLee <wjdgkr0527@naver.com>
2023-01-05 01:31:12 +0900

    feat: Store Entity 구현

---
+++ src/main/java/com/dinosaur/foodbowl/domain/store/entity/Store.java
@@ -0,0 +1,41 @@
+package com.dinosaur.foodbowl.domain.store.entity;
+
+import com.dinosaur.foodbowl.domain.address.entity.Address;
+import com.dinosaur.foodbowl.domain.category.entity.Category;
+import com.dinosaur.foodbowl.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "store")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store extends BaseEntity {
+  private static final int MAX_STORE_NAME_LENGTH = 100;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name="address_id", nullable = false)
+  private Address address;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name="category_id", nullable = false)
+  private Category category;
+
+  @Column(name = "store_name", length = MAX_STORE_NAME_LENGTH)
+  private String storeName;
+
+}
