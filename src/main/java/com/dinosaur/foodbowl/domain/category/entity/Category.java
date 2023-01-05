233ca4bc581dbabaed3483eb501576d8211b8414
woo-chang <clllickme@naver.com>
2023-01-06 08:48:07 +0900

    feat: Category Entity 구현
    issue: #41

--- src/main/java/com/dinosaur/foodbowl/domain/category/entity/Category.java
+++ src/main/java/com/dinosaur/foodbowl/domain/category/entity/Category.java
@@ -5,12 +5,30 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"})
 public class Category {
+
+  private static final int NAME_LENGTH = 45;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", nullable = false, updatable = false)
   private Long id;
+
+  @Column(name = "name", nullable = false, unique = true, length = NAME_LENGTH)
+  private String name;
+
+  @Builder
+  private Category(String name) {
+    this.name = name;
+  }
 }
