5ed1c90535e2657443338071f9f41857a61f9163
woo-chang <clllickme@naver.com>
2023-01-06 07:45:08 +0900

    feat: Address Entity 구현
    issue: #9

--- src/main/java/com/dinosaur/foodbowl/domain/address/entity/Address.java
+++ src/main/java/com/dinosaur/foodbowl/domain/address/entity/Address.java
@@ -1,17 +1,77 @@
 package com.dinosaur.foodbowl.domain.address.entity;
 
 import com.dinosaur.foodbowl.global.entity.BaseEntity;
+import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
+@Table(name = "address")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"})
 public class Address extends BaseEntity {
+
+  private static final int ADDRESS_NAME_LENGTH = 512;
+  private static final int REGION_DEPTH_NAME_LENGTH = 45;
+  private static final int ROAD_NAME_LENGTH = 45;
+  private static final int BUILDING_NO_LENGTH = 45;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", nullable = false, updatable = false)
   private Long id;
+
+  @Column(name = "address_name", nullable = false, length = ADDRESS_NAME_LENGTH)
+  private String addressName;
+
+  @Column(name = "region_1depth_name", nullable = false, length = REGION_DEPTH_NAME_LENGTH)
+  private String region1depthName;
+
+  @Column(name = "region_2depth_name", nullable = false, length = REGION_DEPTH_NAME_LENGTH)
+  private String region2depthName;
+
+  @Column(name = "region_3depth_name", nullable = false, length = REGION_DEPTH_NAME_LENGTH)
+  private String region3depthName;
+
+  @Column(name = "road_name", nullable = false, length = ROAD_NAME_LENGTH)
+  private String roadName;
+
+  @Column(name = "main_building_no", nullable = false, length = BUILDING_NO_LENGTH)
+  private String mainBuildingNo;
+
+  @Column(name = "sub_building_no", nullable = false, length = BUILDING_NO_LENGTH)
+  private String subBuildingNo;
+
+  @Column(name = "longitude")
+  private BigDecimal longitude;
+
+  @Column(name = "latitude")
+  private BigDecimal latitude;
+
+  @Builder
+  private Address(
+      String addressName, String region1depthName, String region2depthName,
+      String region3depthName, String roadName, String mainBuildingNo, String subBuildingNo,
+      BigDecimal longitude, BigDecimal latitude
+  ) {
+    this.addressName = addressName;
+    this.region1depthName = region1depthName;
+    this.region2depthName = region2depthName;
+    this.region3depthName = region3depthName;
+    this.roadName = roadName;
+    this.mainBuildingNo = mainBuildingNo;
+    this.subBuildingNo = subBuildingNo;
+    this.longitude = longitude;
+    this.latitude = latitude;
+  }
 }
