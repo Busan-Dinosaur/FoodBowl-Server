0c27cd35dacdefa6be6b8f749a3dd6400416d11e
woo-chang <clllickme@naver.com>
2023-01-05 19:04:32 +0900

    feat: post repository 기능 구현

---
+++ src/main/java/com/dinosaur/foodbowl/domain/post/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.dinosaur.foodbowl.domain.post.repository;
+
+import com.dinosaur.foodbowl.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
