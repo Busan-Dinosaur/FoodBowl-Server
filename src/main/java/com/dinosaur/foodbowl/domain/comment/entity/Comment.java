ce6a4dd2c6885b2308d3e61b64f96631b2957c46
woo-chang <clllickme@naver.com>
2023-01-06 08:13:00 +0900

    feat: Comment Entity 구현
    issue: #12

---
+++ src/main/java/com/dinosaur/foodbowl/domain/comment/entity/Comment.java
@@ -0,0 +1,63 @@
+package com.dinosaur.foodbowl.domain.comment.entity;
+
+import com.dinosaur.foodbowl.domain.user.entity.User;
+import com.dinosaur.foodbowl.global.entity.BaseEntity;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "comment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"})
+@EntityListeners(AuditingEntityListener.class)
+public class Comment extends BaseEntity {
+
+  private static final int MESSAGE_LENGTH = 255;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", updatable = false, nullable = false)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_id")
+  private Comment comment;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id", nullable = false)
+  private Post post;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(name = "message", nullable = false, length = MESSAGE_LENGTH)
+  private String message;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @Builder
+  private Comment(Comment comment, Post post, User user, String message) {
+    this.comment = comment;
+    this.post = post;
+    this.user = user;
+    this.message = message;
+  }
+}
