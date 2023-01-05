cb557daf958869fc8275d9e1c681b241f6134238
woo-chang <clllickme@naver.com>
2023-01-06 08:19:41 +0900

    refactor: 무분별한 getter 사용 지양을 위한 @Getter 제거

--- src/main/java/com/dinosaur/foodbowl/domain/address/entity/Address.java
+++ src/main/java/com/dinosaur/foodbowl/domain/address/entity/Address.java
@@ -11,10 +11,8 @@ import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
 @Entity
 @Table(name = "address")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
