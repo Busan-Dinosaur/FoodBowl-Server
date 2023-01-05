c1d517bf3821dbaea9be0a9097fb5b1a5b74c10d
woo-chang <clllickme@naver.com>
2023-01-06 07:49:02 +0900

    refactor: repository -> dao 디렉토리명 수정

--- src/main/java/com/dinosaur/foodbowl/domain/post/dao/PostRepository.java
+++ src/main/java/com/dinosaur/foodbowl/domain/post/dao/PostRepository.java
@@ -1,4 +1,4 @@
-package com.dinosaur.foodbowl.domain.post.repository;
+package com.dinosaur.foodbowl.domain.post.dao;
 
 import com.dinosaur.foodbowl.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
