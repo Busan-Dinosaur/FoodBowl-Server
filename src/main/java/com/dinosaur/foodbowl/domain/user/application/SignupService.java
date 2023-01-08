95406f99612e03dde65b0d62ba56a8bbd303f7e9
정핸모 <gusah009@naver.com>
2023-01-08 16:54:50 +0900

    refactor: 회원가입 코드 리팩토링

--- src/main/java/com/dinosaur/foodbowl/domain/user/application/SignupService.java
+++ src/main/java/com/dinosaur/foodbowl/domain/user/application/SignupService.java
@@ -5,12 +5,14 @@ import com.dinosaur.foodbowl.domain.user.dao.UserRepository;
 import com.dinosaur.foodbowl.domain.user.dto.request.SignupRequestDto;
 import com.dinosaur.foodbowl.domain.user.dto.response.SignupResponseDto;
 import com.dinosaur.foodbowl.domain.user.entity.User;
+import com.dinosaur.foodbowl.domain.user.entity.role.Role.RoleType;
 import com.dinosaur.foodbowl.global.config.security.JwtTokenProvider;
 import com.dinosaur.foodbowl.global.util.thumbnail.ThumbnailUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +26,17 @@ public class SignupService {
 
   @Transactional
   public SignupResponseDto signup(SignupRequestDto request) {
-    Thumbnail userThumbnail = null;
-    if (request.getThumbnail() != null) {
-      userThumbnail = thumbnailUtil.save(request.getThumbnail());
-    }
+    Thumbnail userThumbnail = saveThumbnailIfExist(request.getThumbnail());
     User user = userRepository.save(request.toEntity(userThumbnail, passwordEncoder));
-    String accessToken = jwtTokenProvider.createToken(user.getId(), "ROLE_USER");
+    String accessToken = jwtTokenProvider.createToken(user.getId(), RoleType.USER.getName());
     return SignupResponseDto.of(user, accessToken);
+  }
+
+  private Thumbnail saveThumbnailIfExist(MultipartFile thumbnail) {
+    Thumbnail userThumbnail = null;
+    if (thumbnail != null) {
+      userThumbnail = thumbnailUtil.save(thumbnail);
+    }
+    return userThumbnail;
   }
 }
