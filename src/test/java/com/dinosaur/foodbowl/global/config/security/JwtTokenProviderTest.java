134e7d3c30eb5864a5243b22a2a3bf84eda89398
정핸모 <gusah009@naver.com>
2023-01-10 09:34:40 +0900

    test: 유저가 여러 역할을 가질 수 있게 JWT 구성 변경 테스트 추가

--- src/test/java/com/dinosaur/foodbowl/global/config/security/JwtTokenProviderTest.java
+++ src/test/java/com/dinosaur/foodbowl/global/config/security/JwtTokenProviderTest.java
@@ -1,26 +1,67 @@
 package com.dinosaur.foodbowl.global.config.security;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.dinosaur.foodbowl.domain.user.entity.role.Role.RoleType;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ContextConfiguration
 class JwtTokenProviderTest {
 
-  @Test
-  void createAccessToken() {
+  private static final String TEST_SECRET_KEY = "foodbowl_zzang_zzang";
+  private static final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider();
+
+  @BeforeAll
+  static void setupSecretKey() {
+    ReflectionTestUtils.setField(jwtTokenProvider, "secretKey", TEST_SECRET_KEY);
   }
 
   @Test
-  void testCreateAccessToken() {
+  void createAccessToken() {
+    long userPk = 1;
+    RoleType[] roleType = new RoleType[]{RoleType.USER, RoleType.ADMIN};
+
+    String accessToken = jwtTokenProvider.createAccessToken(userPk, roleType);
+    accessToken = removePrefix(accessToken);
+    Claims claims = Jwts.parser().setSigningKey(TEST_SECRET_KEY)
+        .parseClaimsJws(accessToken)
+        .getBody();
+
+    long resultUserPK = Long.parseLong(claims.getSubject());
+    String roles = claims.get("roles").toString();
+    assertThat(resultUserPK).isEqualTo(userPk);
+    assertThat(roles).isEqualTo("ROLE_USER,ROLE_ADMIN");
+  }
+
+  private String removePrefix(String accessToken) {
+    return accessToken.split(" ")[1];
   }
 
   @Test
   void getAuthentication() {
-  }
+    /**
+     * PK: 1
+     * ROLE: USER, ADMIN
+     */
+    String validToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOiJST0xFX1VTRVIsUk9MRV9BRE1JTiIsImlhdCI6MTY3MzMwOTg3MSwiZXhwIjoxNjczMzEzNDcxfQ.pSVOJM9LuBVXOmwF5PAmEhSASUE5HeSJZpQ15gGd2o0";
 
-  @Test
-  void tryCheckTokenValid() {
-  }
+    Authentication authentication = jwtTokenProvider.getAuthentication(validToken);
 
-  @Test
-  void resolveToken() {
+    assertThat(authentication.isAuthenticated()).isTrue();
+    assertThat(authentication.getPrincipal()).isInstanceOf(UserDetails.class);
+    List<String> authorities = authentication.getAuthorities().stream()
+        .map(GrantedAuthority::getAuthority)
+        .collect(Collectors.toList());
+    assertThat(authorities).containsAll(List.of("ROLE_USER", "ROLE_ADMIN"));
   }
 }
