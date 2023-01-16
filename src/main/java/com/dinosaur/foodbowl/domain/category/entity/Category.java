b15da32388fd758809898caa99863eef6447f976
woo-chang <clllickme@naver.com>
2023-01-16 18:28:41 +0900

    refactor: DB 동등성 유지를 위한 카테고리 수정

--- src/main/java/com/dinosaur/foodbowl/domain/category/entity/Category.java
+++ src/main/java/com/dinosaur/foodbowl/domain/category/entity/Category.java
@@ -32,36 +32,24 @@ public class Category {
   private CategoryType categoryType;
 
   public enum CategoryType {
-    족발_보쌈(1, "족발∙보쌈"),
-    찜_탕_찌개(2, "찜∙탕∙찌개"),
-    돈까스_회_일식(3, "돈까스∙회∙일식"),
-    피자(4, "피자"),
-    고기_구이(5, "고기∙구이"),
-    야식(6, "야식"),
-    양식(7, "양식"),
-    치킨(8, "치킨"),
-    중식(9, "중식"),
-    아시안(10, "아시안"),
-    백반_죽_국수(11, "백반∙죽∙국수"),
-    도시락(12, "도시락"),
-    분식(13, "분식"),
-    카페_디저트(14, "카페∙디저트"),
-    패스트푸드(15, "패스트푸드");
+    카페(1L),
+    한식(2L),
+    양식(3L),
+    일식(4L),
+    중식(5L),
+    치킨(6L),
+    분식(7L),
+    해산물(8L),
+    샐러드(9L);
 
     private final long id;
-    private final String name;
 
-    CategoryType(long id, String name) {
+    CategoryType(long id) {
       this.id = id;
-      this.name = name;
     }
 
     public long getId() {
       return id;
-    }
-
-    public String getName() {
-      return name;
     }
   }
 }
