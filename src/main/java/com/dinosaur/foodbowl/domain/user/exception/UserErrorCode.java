9100789053ea1fce732b13dd48be6761a99a4d6f
woo-chang <clllickme@naver.com>
2023-01-18 23:24:20 +0900

    fix: Http 스펙으로 인한 bad request -> not found 상태 수정

--- src/main/java/com/dinosaur/foodbowl/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/dinosaur/foodbowl/domain/user/exception/UserErrorCode.java
@@ -8,7 +8,7 @@ public enum UserErrorCode {
 
   LOGIN_ID_DUPLICATE("이미 존재하는 로그인 아이디입니다.", HttpStatus.CONFLICT),
   NICKNAME_DUPLICATE("이미 존재하는 닉네임입니다.", HttpStatus.CONFLICT),
-  USER_NOT_FOUND("유저를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
+  USER_NOT_FOUND("유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   ;
 
   private final String message;
