45b9d6363ef0a4eeae1853872a61e8694648bdc7
JeonghakLee <wjdgkr0527@naver.com>
2023-01-18 21:28:53 +0900

    refactor: 중복코드 userTestHelper 메서드 사용하도록 수정

--- src/test/java/com/dinosaur/foodbowl/domain/follow/application/FollowServiceTest.java
+++ src/test/java/com/dinosaur/foodbowl/domain/follow/application/FollowServiceTest.java
@@ -1,20 +1,12 @@
 package com.dinosaur.foodbowl.domain.follow.application;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
-
-import static com.dinosaur.foodbowl.domain.user.entity.User.MAX_INTRODUCE_LENGTH;
-import static com.dinosaur.foodbowl.domain.user.entity.User.MAX_LOGIN_ID_LENGTH;
-import static com.dinosaur.foodbowl.domain.user.entity.User.MAX_NICKNAME_LENGTH;
-import static com.dinosaur.foodbowl.domain.user.entity.User.MAX_PASSWORD_LENGTH;
 
 import com.dinosaur.foodbowl.domain.follow.dao.FollowRepository;
-import com.dinosaur.foodbowl.domain.thumbnail.entity.Thumbnail;
-import com.dinosaur.foodbowl.domain.user.dao.UserRepository;
+import com.dinosaur.foodbowl.domain.user.UserTestHelper;
 import com.dinosaur.foodbowl.domain.user.entity.User;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
-import java.util.UUID;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -30,12 +22,11 @@ class FollowServiceTest {
   @Autowired
   EntityManager em;
   @Autowired
-  private UserRepository userRepository;
-
-  @Autowired
   private FollowRepository followRepository;
   @Autowired
   private FollowService followService;
+  @Autowired
+  private UserTestHelper userTestHelper;
 
   @Nested
   @DisplayName("성공 테스트")
@@ -45,8 +36,8 @@ class FollowServiceTest {
     @DisplayName("로그인한 유저와 팔로우 할 유저가 존재하면 팔로잉을 성공한다.")
     void shouldSucceedToFollowWhenValidatedUsers() {
       // given
-      User me = generateUser(null);
-      User other = generateUser(null);
+      User me = userTestHelper.generateUserWithoutThumbnail();
+      User other = userTestHelper.generateUserWithoutThumbnail();
 
       // when
       followService.follow(me, other.getId());
@@ -62,8 +53,8 @@ class FollowServiceTest {
     @DisplayName("팔로잉한 유저이면 팔로잉 취소는 성공한다.")
     void shouldSucceedToUnfollowWhenFollowing() {
       // given
-      User me = generateUser(null);
-      User other = generateUser(null);
+      User me = userTestHelper.generateUserWithoutThumbnail();
+      User other = userTestHelper.generateUserWithoutThumbnail();
       followService.follow(me, other.getId());
       em.flush();
       em.clear();
@@ -85,7 +76,7 @@ class FollowServiceTest {
     @DisplayName("팔로잉 대상이 자신이면 실패한다.")
     void shouldFailToFollowWhenOneself() {
       // given
-      User me = generateUser(null);
+      User me = userTestHelper.generateUserWithoutThumbnail();
 
       // then
       assertThatThrownBy(() -> followService.follow(me, me.getId())).isInstanceOf(
@@ -97,8 +88,8 @@ class FollowServiceTest {
     @DisplayName("이미 팔로잉한 유저이면 팔로잉은 실패한다.")
     void shouldFailToFollowWhenAlreadyFollowed() {
       // given
-      User me = generateUser(null);
-      User other = generateUser(null);
+      User me = userTestHelper.generateUserWithoutThumbnail();
+      User other = userTestHelper.generateUserWithoutThumbnail();
       followService.follow(me, other.getId());
       em.flush();
       em.clear();
@@ -113,7 +104,7 @@ class FollowServiceTest {
     @DisplayName("팔로잉 취소 대상이 자신이면 실패한다.")
     void shouldFailToUnfollowWhenOneself() {
       // given
-      User me = generateUser(null);
+      User me = userTestHelper.generateUserWithoutThumbnail();
 
       // then
       assertThatThrownBy(() -> followService.unfollow(me, me.getId())).isInstanceOf(
@@ -125,8 +116,8 @@ class FollowServiceTest {
     @DisplayName("이미 팔로잉한 유저가 아니면 팔로잉 취소는 실패한다.")
     void shouldSucceedToUnfollowWhenFollowing() {
       // given
-      User me = generateUser(null);
-      User other = generateUser(null);
+      User me = userTestHelper.generateUserWithoutThumbnail();
+      User other = userTestHelper.generateUserWithoutThumbnail();
       em.flush();
       em.clear();
 
@@ -135,22 +126,5 @@ class FollowServiceTest {
           IllegalArgumentException.class);
     }
 
-  }
-
-  private User generateUser(Thumbnail thumbnail) {
-    User userWithThumbnail = User.builder()
-        .loginId(getRandomUUIDLengthWith(MAX_LOGIN_ID_LENGTH))
-        .nickname(getRandomUUIDLengthWith(MAX_NICKNAME_LENGTH))
-        .password(getRandomUUIDLengthWith(MAX_PASSWORD_LENGTH))
-        .introduce(getRandomUUIDLengthWith(MAX_INTRODUCE_LENGTH))
-        .thumbnail(thumbnail)
-        .build();
-    return userRepository.save(userWithThumbnail);
-  }
-
-  private static String getRandomUUIDLengthWith(int length) {
-    String randomString = UUID.randomUUID().toString();
-    length = Math.min(length, randomString.length());
-    return randomString.substring(0, length);
   }
 }
