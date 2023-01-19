bc106e17117366166d6c63c9d7ba72cef64198f8
jeonghak <wjdgkr0527@naver.com>
2023-01-19 10:35:46 +0900

    test: 자신에 대한 팔로우, 팔로우 취소 요청 검증 테스트 구현

--- src/test/java/com/dinosaur/foodbowl/domain/follow/api/FollowControllerTest.java
+++ src/test/java/com/dinosaur/foodbowl/domain/follow/api/FollowControllerTest.java
@@ -2,6 +2,7 @@ package com.dinosaur.foodbowl.domain.follow.api;
 
 import static com.dinosaur.foodbowl.global.config.security.JwtTokenProvider.ACCESS_TOKEN;
 import static com.dinosaur.foodbowl.global.config.security.JwtTokenProvider.DEFAULT_TOKEN_VALID_MILLISECOND;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
 import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -12,17 +13,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.dinosaur.foodbowl.domain.follow.application.FollowService;
+import com.dinosaur.foodbowl.domain.user.entity.User;
 import com.dinosaur.foodbowl.domain.user.entity.role.Role.RoleType;
 import com.dinosaur.foodbowl.global.api.ControllerTest;
 import com.dinosaur.foodbowl.global.util.auth.AuthUtil;
 import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest(FollowController.class)
@@ -37,14 +40,23 @@ public class FollowControllerTest extends ControllerTest {
   @DisplayName("팔로우 & 언팔로우")
   class FollowAndUnfollow {
 
-    private final Long userId = 1L;
+    private final Long myId = 1L;
     private final Long otherId = 2L;
-    private final String userToken = jwtTokenProvider.createAccessToken(userId, RoleType.ROLE_회원);
+    private final String userToken = jwtTokenProvider.createAccessToken(myId, RoleType.ROLE_회원);
+
+    @BeforeEach
+    void setup() {
+      User user = User.builder().build();
+      ReflectionTestUtils.setField(user, "id", myId);
+      when(authUtil.getUserByJWT()).thenReturn(user);
+      when(authUtil.getUserIdByJWT()).thenReturn(myId);
+
+    }
 
     @Test
-    @DisplayName("팔로잉을 성공하면 204")
+    @DisplayName("팔로우 요청을 성공하면 204 NO Content 를 반환한다.")
     void shouldSucceedFollowWhenValidatedUsers() throws Exception {
-      callFollowApi().andExpect(status().isNoContent())
+      callFollowApi(otherId).andExpect(status().isNoContent())
           .andDo(print())
           .andDo(document("follow",
               requestCookies(
@@ -58,9 +70,9 @@ public class FollowControllerTest extends ControllerTest {
     }
 
     @Test
-    @DisplayName("언팔로우를 성공하면 204")
+    @DisplayName("팔로우 취소 요청을 성공하면 204 NO Content 를 반환한다.")
     void shouldSucceedUnfollowWhenValidatedUsers() throws Exception {
-      callUnfollowApi().andExpect(status().isNoContent())
+      callUnfollowApi(otherId).andExpect(status().isNoContent())
           .andDo(print())
           .andDo(document("unfollow",
               requestCookies(
@@ -72,6 +84,19 @@ public class FollowControllerTest extends ControllerTest {
                   parameterWithName("userId").description("팔로우를 취소할 유저의 아이디")
               )));
     }
+
+    @Test
+    @DisplayName("자신에 대한 팔로우 요청은 400 Bad Request 를 반환한다.")
+    void shouldClientErrorWhenFollowOneself() throws Exception {
+      callFollowApi(myId).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("자신에 대한 팔로우 취소 요청은 400 Bad Request 를 반환한다.")
+    void shouldClientErrorWhenUnfollowOneself() throws Exception {
+      callUnfollowApi(myId).andExpect(status().isBadRequest());
+    }
+
 
     @Test
     @DisplayName("토큰이 없을 경우 팔로우는 실패한다.")
@@ -88,14 +113,14 @@ public class FollowControllerTest extends ControllerTest {
     }
 
 
-    private ResultActions callFollowApi() throws Exception {
-      return mockMvc.perform(post("/follows/{userId}", otherId)
+    private ResultActions callFollowApi(Long userId) throws Exception {
+      return mockMvc.perform(post("/follows/{userId}", userId)
           .cookie(new Cookie(ACCESS_TOKEN, userToken))
           .contentType(MediaType.APPLICATION_JSON));
     }
 
-    private ResultActions callUnfollowApi() throws Exception {
-      return mockMvc.perform(delete("/follows/{userId}", otherId)
+    private ResultActions callUnfollowApi(Long userId) throws Exception {
+      return mockMvc.perform(delete("/follows/{userId}", userId)
           .cookie(new Cookie(ACCESS_TOKEN, userToken))
           .contentType(MediaType.APPLICATION_JSON));
     }
