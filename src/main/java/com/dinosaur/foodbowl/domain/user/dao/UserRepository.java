9fa2cb8f42296daafe7c47cccc19ffcf89aa4150
woo-chang <clllickme@naver.com>
2023-01-20 13:43:14 +0900

    feat: 로그인 아이디로 유저를 찾는 기능 구현

--- src/main/java/com/dinosaur/foodbowl/domain/user/dao/UserRepository.java
+++ src/main/java/com/dinosaur/foodbowl/domain/user/dao/UserRepository.java
@@ -1,6 +1,7 @@
 package com.dinosaur.foodbowl.domain.user.dao;
 
 import com.dinosaur.foodbowl.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -8,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
   boolean existsByLoginId(String loginId);
 
   boolean existsByNickname(String nickname);
+
+  Optional<User> findByLoginId(String loginId);
 }
