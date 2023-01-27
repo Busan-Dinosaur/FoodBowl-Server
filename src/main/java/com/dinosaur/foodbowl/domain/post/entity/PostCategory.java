ba96981b8ef9115e234b56527fc3b75622e876dd
woo-chang <clllickme@naver.com>
2023-01-27 22:52:45 +0900

    feat: 게시글 카테고리 복수 선택 엔티티 생성

---
+++ src/main/java/com/dinosaur/foodbowl/domain/post/entity/PostCategory.java
@@ -0,0 +1,43 @@
+package com.dinosaur.foodbowl.domain.post.entity;
+
+import com.dinosaur.foodbowl.domain.category.entity.Category;
+import com.dinosaur.foodbowl.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post_category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+public class PostCategory extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id", nullable = false, updatable = false)
+  private Post post;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id", nullable = false, updatable = false)
+  private Category category;
+
+  @Builder
+  private PostCategory(Post post, Category category) {
+    this.post = post;
+    this.category = category;
+  }
+}
