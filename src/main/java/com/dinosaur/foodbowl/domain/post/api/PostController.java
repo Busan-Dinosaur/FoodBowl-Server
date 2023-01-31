5b70bd9ebf6a9152c1ca35e2fd8e40151c1016c6
JeonghakLee <wjdgkr0527@naver.com>
2023-02-01 05:29:59 +0900

    refactor: 불필요한 dto 제거

--- src/main/java/com/dinosaur/foodbowl/domain/post/api/PostController.java
+++ src/main/java/com/dinosaur/foodbowl/domain/post/api/PostController.java
@@ -2,16 +2,12 @@ package com.dinosaur.foodbowl.domain.post.api;
 
 import com.dinosaur.foodbowl.domain.post.application.PostService;
 import com.dinosaur.foodbowl.domain.post.dto.PostCreateRequestDto;
-import com.dinosaur.foodbowl.domain.post.dto.PostCreateResponseDto;
 import com.dinosaur.foodbowl.domain.user.entity.User;
 import com.dinosaur.foodbowl.global.util.auth.AuthUtil;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +24,7 @@ public class PostController {
   private final PostService postService;
 
   @PostMapping
-  public ResponseEntity<PostCreateResponseDto> createPost(
+  public ResponseEntity<Void> createPost(
       @RequestPart(required = true) List<MultipartFile> images,
       @Valid @RequestPart PostCreateRequestDto request) {
     User me = authUtil.getUserByJWT();
