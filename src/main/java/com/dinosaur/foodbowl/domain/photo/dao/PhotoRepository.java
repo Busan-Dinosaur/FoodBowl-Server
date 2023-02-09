f352bc7bade3619d69ee9e444b1ebb911cf4b93c
JeonghakLee <wjdgkr0527@naver.com>
2023-02-10 00:50:09 +0900

    refactor: 요상한 흔적 지우기

--- src/main/java/com/dinosaur/foodbowl/domain/photo/dao/PhotoRepository.java
+++ src/main/java/com/dinosaur/foodbowl/domain/photo/dao/PhotoRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
-  @Override
-  boolean existsById(Long aLong);
 }
