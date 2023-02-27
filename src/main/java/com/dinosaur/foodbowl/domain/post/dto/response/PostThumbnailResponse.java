229fe5ebd1205dd5b7a9e53c5a54107d2c8744c7
woo-chang <clllickme@naver.com>
2023-02-28 00:19:00 +0900

    feat: 특정 사용자 게시글을 제외한 모든 게시글 썸네일 목록 조회 기능 구현

---
+++ src/main/java/com/dinosaur/foodbowl/domain/post/dto/response/PostThumbnailResponse.java
@@ -0,0 +1,12 @@
+package com.dinosaur.foodbowl.domain.post.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import java.time.LocalDateTime;
+
+public record PostThumbnailResponse(
+    Long postId,
+    String thumbnailPath,
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul") LocalDateTime createdAt) {
+
+}
